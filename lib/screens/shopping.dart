c3363b21648581e5b2ac3d443b0cf0218b486687
Jesse Nehemiah <nehemiahjess@gmail.com>
2024-09-05 17:00:44 +0300

    redesigned the shopping page

--- lib/screens/shopping.dart
+++ lib/screens/shopping.dart
@@ -141,66 +141,117 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
       ),
       children: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              margin: EdgeInsets.all(10.0),
+              color: Colors.black,
+              child: ListTile(
+                leading: Icon(Icons.start_outlined),
+                textColor: Colors.white,
+                title: Text('Click the plus button to create a new list'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(children: [
+              ExpansionTile(
+                leading: Icon(
+                  Icons.list_alt_outlined,
+                  color: Colors.black,
+                ),
+                initiallyExpanded: true,
+                title: Text(
+                  'Current Shopping Lists',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                children: [
+                  ..._shoppingList.map(
+                    (item) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                          margin: EdgeInsets.all(10.0),
+                          color: Colors.black,
+                          child: ListTile(
+                            leading:
+                                Icon(Icons.shopping_bag, color: Colors.white),
+                            title: Text(
+                              item['product'],
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            subtitle: Text(
+                              'Brand: ${item['brand']}, Quantity: ${item['qt']}, Supermarket: ${item['supermarket']}',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            trailing: Text(
+                              '\$${item['price']}',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          ),
           Expanded(
             child: ListView(
               children: [
-                if (_checkoutHistory.isNotEmpty) ...[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Checkout History',
-                    ),
-                  ),
-                  ..._checkoutHistory.map((checkout) => ListTile(
-                        leading: Icon(Icons.receipt),
-                        title: Text(
-                            'Checkout on ${DateTime.parse(checkout['checkout_date']).toLocal()}'),
-                        subtitle: Text(
-                            'Total: \$${checkout['total_amount'].toStringAsFixed(2)}'),
-                        trailing: Icon(Icons.arrow_forward_ios),
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => CheckoutDetailScreen(
-                                checkoutData: checkout,
-                              ),
-                            ),
-                          );
-                          // TODO: Navigate to a detailed view of this checkout
-                        },
-                      )),
-                  Divider(),
-                ],
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    margin: EdgeInsets.all(10.0),
+                ExpansionTile(
+                  leading: Icon(
+                    Icons.history_outlined,
                     color: Colors.black,
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.list_alt_outlined,
-                        color: Colors.white,
-                      ),
-                      title: Text(
-                        'Current Shopping List',
-                        style: TextStyle(
-                          color: Colors.white,
+                  ),
+                  title: Text('Checkout History'),
+                  children: [
+                    if (_checkoutHistory.isNotEmpty) ...[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Checkout History',
                         ),
                       ),
-                      subtitle: Text(
-                        'Click the plus button to create one',
-                        style: TextStyle(color: Colors.white),
+                      ..._checkoutHistory.map(
+                        (checkout) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            margin: EdgeInsets.all(10.0),
+                            color: Colors.black,
+                            child: ListTile(
+                              leading: Icon(Icons.receipt),
+                              title: Text(
+                                'Checkout on ${DateTime.parse(checkout['checkout_date']).toLocal()}',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              subtitle: Text(
+                                'Total: \$${checkout['total_amount'].toStringAsFixed(2)}',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              trailing: Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.white,
+                              ),
+                              tileColor: Colors.black,
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => CheckoutDetailScreen(
+                                      checkoutData: checkout,
+                                    ),
+                                  ),
+                                );
+                                // TODO: Navigate to a detailed view of this checkout
+                              },
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
+                      Divider(),
+                    ],
+                  ],
                 ),
-                ..._shoppingList.map((item) => ListTile(
-                      leading: Icon(Icons.shopping_bag),
-                      title: Text(item['product']),
-                      subtitle: Text(
-                          'Brand: ${item['brand']}, Quantity: ${item['qt']}, Supermarket: ${item['supermarket']}'),
-                      trailing: Text('\$${item['price']}'),
-                    )),
               ],
             ),
           ),
